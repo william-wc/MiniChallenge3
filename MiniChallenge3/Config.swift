4d313e5b90f5d5963e9d5b80efa2ba46a890ee2c
William Cho <williamhjcho@gmail.com>
2015-05-22 14:04:56 -0300

    added json

---
+++ MiniChallenge3/Config.swift
@@ -0,0 +1,17 @@
+//
+//  Config.swift
+//  MiniChallenge3
+//
+//  Created by William Hong Jun Cho on 5/22/15.
+//  Copyright (c) 2015 William Hong Jun Cho. All rights reserved.
+//
+
+import Foundation
+
+
+class Config {
+    
+    
+    
+    
+}
