f0a08fcf5a268b4e21d0f8b0a7211f6f281518f4
victorhml <31203078@mackenzista.com.br>
2015-05-26 17:26:15 -0300

    Quiz implementação

--- MiniChallenge3/QuizViewController.swift
+++ MiniChallenge3/QuizViewController.swift
@@ -11,36 +11,67 @@ import UIKit
 
 extension Array{
     mutating func shuffle(){
-        for _ in 0...alt.count-1 {
+        for _ in 0...self.count-1 {
             sort { (_,_) in arc4random() < arc4random() }
         }
     }
 }
 
-var alt = ["a", "b", "c", "d"]
+class quizData {
+    var questions: String!
+    var alt: [String]!
+    init (questions: String, alt: [String]){
+        self.questions = questions
+        self.alt = alt
+    }
+}
 
 
 class QuizViewController:  UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     
+    var list = [quizData(questions: "Quanto é 1+1?", alt: ["2", "4", "1", "3"]),
+        quizData(questions: "O iPhone pertence a qual empresa?", alt: ["Apple", "Samsung", "Microsoft", "Google"]),quizData(questions: "Quantos anos vc tem?", alt: ["21", "19", "31", "24"]),quizData(questions: "Qual a derivada de x?", alt: ["1", "2x", "0", "x"])]
+
+    
     @IBOutlet var question: UITextView!
+    var pos: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.question.editable = false
-        self.question.text = "Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste? Pergunta Teste?"
-        println("\(alt)")
-        alt.shuffle()
-        println("\(alt)")
+        var right = list[0].alt[0]
+        
+        list[0].alt.shuffle()
+        
+        for i in 0...list[0].alt.count-1{
+            if right == list[0].alt[i]{
+                pos = i
+            }
+        }
+        
+        println("\(pos)")
+        
+        self.question.text = list[0].questions
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! QuizCell
-        cell.options.text = alt[indexPath.row]
+        cell.options.text = list[0].alt[indexPath.row]
         return cell
     }
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return alt.count
-    }
-
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var cell = tableView.cellForRowAtIndexPath(indexPath)
+        if indexPath.row == pos {
+            cell?.backgroundColor = UIColor.greenColor()
+        }
+        else{
+            cell?.backgroundColor = UIColor.redColor()
+            self.performSegueWithIdentifier("wrong", sender: self)
+        }
+    }
 }
