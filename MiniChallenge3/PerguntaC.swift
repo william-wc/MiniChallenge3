ac9df5f216d6557361b7dd261bf04b2b61762297
Bruno Faganello Neto <bruno.faganello.neto@gmail.com.com>
2015-05-27 14:10:20 -0300

    Conversão array to binary data e vice - versa

--- MiniChallenge3/PerguntaC.swift
+++ MiniChallenge3/PerguntaC.swift
@@ -15,9 +15,27 @@ class PerguntaC: NSManagedObject {
     @NSManaged var questao: String
     @NSManaged var resolucao: String
     @NSManaged var index: Int16
-    @NSManaged var alt1: String
-    @NSManaged var alt2: String
-    @NSManaged var alt3: String
-    @NSManaged var alt4: String
+    @NSManaged var alternativas: NSData
     
+    
+
+    func convertToBinaryData(alts:[String]) -> NSData{
+        
+        let data = NSData(bytes: alts, length: alts.count * sizeof(String))
+        
+        return data
+        
+    }
+    
+    func convertToArray(data:NSData) -> [String]{
+        let count = data.length / sizeof(String)
+        
+        var array = [AnyObject](count:count, repeatedValue: 0)
+        
+        data.getBytes(&array, length:count * sizeof(String))
+    
+        return array as! [String]
+    }
+
+
 }
